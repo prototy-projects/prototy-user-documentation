72e97a6b561b07b0c1ee18050cc022d6da67f695
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2025-01-14 11:28:00 +0100

    🎉 Init docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/jwoirhay/Bureau/prototy/prototy-user-documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/home/jwoirhay/Bureau/prototy/prototy-user-documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/jwoirhay/Bureau/prototy/prototy-user-documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/jwoirhay/Bureau/prototy/prototy-user-documentation/src/css/custom.css"),
+];
